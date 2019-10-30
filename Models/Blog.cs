9737727eb47728907dbfd1bec7c9f9b4df0c3955
amahdavi9503 <amahdavi9503@gmail.com>
2019-10-30 10:13:26 -0500

    1) Created Data Model: Blog, Post, Blogging Context
    2) Created Program.cs

---
+++ Models/Blog.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BlogsConsole.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+}
+
---
+++ Models/BloggingContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+
+namespace BlogsConsole.Models
+{
+    public class BloggingContext : DbContext
+    {
+        //Create a derived context to create a session with the database
+        public BloggingContext() : base("name=BlogContext") { }
+
+        //DbSet creates a relationshhip between our classes and the database:
+        //  Blogs table is a list (a DbSet) of type Blog
+        //  Posts table is a list (a DbSet) of type Post
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        //This method adds a blog to the list of blogs
+        public void AddBlog(Blog blog)
+        {
+            this.Blogs.Add(blog);  //add a blog DbSet
+            this.SaveChanges();  //updates the database based on DbSet
+        }
+    }
+}
+
