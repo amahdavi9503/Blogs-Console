f338e669911535eee8f8deff58f64a02ef4fade9
amahdavi9503 <amahdavi9503@gmail.com>
2019-10-30 10:54:17 -0500

    Create Program
    1) Prompt User for blog name
    2) Save blog to database
    3) Display all blogs

--- Models/BloggingContext.cs
+++ Models/BloggingContext.cs
@@ -16,6 +16,10 @@ namespace BlogsConsole.Models
         //This method adds a blog to the list of blogs
         public void AddBlog(Blog blog)
         {
+            // "this" referes to a property of this class. Once we create an instance of BloggingContext, 
+            // "this" will refer to its own blogs property that's part of this class 
+            // instead of a local variable called blogs. Use of "this" is optional, but it is good practice
+            // to use, because it lets us know that the property belongs to this object
             this.Blogs.Add(blog);  //add a blog DbSet
             this.SaveChanges();  //updates the database based on DbSet
         }
